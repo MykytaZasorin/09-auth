4d4c5b68b2fed1e08c31d376fd8019ca22c389ed
MykytaZasorin <mykytazasorin@gmail.com>
2026-06-17 22:30:52 +0300

    async proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,7 @@
 // proxy.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 import { checkSession } from "./lib/api/serverApi";
 
 const privateRoutes = ["/profile", "/notes"];
@@ -9,8 +10,9 @@ const publicRoutes = ["/sign-in", "/sign-up"];
 export default async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const refreshToken = request.cookies.get("refreshToken")?.value;
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
 
   let isAuth = !!accessToken;
   let responseWithNewCookies: NextResponse | null = null;
